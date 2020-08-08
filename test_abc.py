579fb7df5c89ecdd41c456f5b26fac1ec16ae72d
Blkrop <69386399+Blkrop@users.noreply.github.com>
2020-08-08 14:25:55 +0300

    Update test_abc.py

--- test_abc.py
+++ test_abc.py
@@ -1,16 +1,16 @@
 import unittest
-import abc
+import generator
 
 
 def test_sample_single_word():
     list_ = ('foo', 'bar', 'foobar')
-    word = abc.sample(list_)
+    word = generator.sample(list_)
     assert word in list_
 
 
 def test_sample_multiple_words():
     list_ = ('foo', 'bar', 'foobar')
-    words = abc.sample(list_, 2)
+    words = generator.sample(list_, 2)
     assert len(words) == 2
     assert words[0] in list_
     assert words[1] in list_
@@ -18,5 +18,5 @@ def test_sample_multiple_words():
 
 
 def test_generate_buzz_of_at_least_five_words():
-    phrase = abc.generate_buzz()
+    phrase = generator.generate_buzz()
     assert len(phrase.split()) >= 5
