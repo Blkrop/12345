caa653042139204887ca0519d34d80825397ce4b
Blkrop <69386399+Blkrop@users.noreply.github.com>
2020-08-08 14:24:01 +0300

    Update and rename test_12345.py to test_abc.py

--- test_abc.py
+++ test_abc.py
@@ -1,16 +1,16 @@
 import unittest
-import 12345
+import abc
 
 
 def test_sample_single_word():
     list_ = ('foo', 'bar', 'foobar')
-    word = 12345.sample(list_)
+    word = abc.sample(list_)
     assert word in list_
 
 
 def test_sample_multiple_words():
     list_ = ('foo', 'bar', 'foobar')
-    words = 12345.sample(list_, 2)
+    words = abc.sample(list_, 2)
     assert len(words) == 2
     assert words[0] in list_
     assert words[1] in list_
@@ -18,5 +18,5 @@ def test_sample_multiple_words():
 
 
 def test_generate_buzz_of_at_least_five_words():
-    phrase = 12345.generate_buzz()
+    phrase = abc.generate_buzz()
     assert len(phrase.split()) >= 5
