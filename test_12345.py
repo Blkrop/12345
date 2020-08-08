9d5471b648da520d0985411a86a5edd33c854609
Blkrop <69386399+Blkrop@users.noreply.github.com>
2020-08-08 14:19:54 +0300

    Update and rename test_generator.py to test_12345.py

--- test_12345.py
+++ test_12345.py
@@ -1,16 +1,16 @@
 import unittest
-import generator
+import 12345
 
 
 def test_sample_single_word():
     list_ = ('foo', 'bar', 'foobar')
-    word = generator.sample(list_)
+    word = 12345.sample(list_)
     assert word in list_
 
 
 def test_sample_multiple_words():
     list_ = ('foo', 'bar', 'foobar')
-    words = generator.sample(list_, 2)
+    words = 12345.sample(list_, 2)
     assert len(words) == 2
     assert words[0] in list_
     assert words[1] in list_
@@ -18,5 +18,5 @@ def test_sample_multiple_words():
 
 
 def test_generate_buzz_of_at_least_five_words():
-    phrase = generator.generate_buzz()
+    phrase = 12345.generate_buzz()
     assert len(phrase.split()) >= 5
